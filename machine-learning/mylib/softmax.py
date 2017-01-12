b42bcaa3384699c1a85bcd489f4a06065d0f5ebc
Munetaka Mizutani <mu.mizutani@gmail.com>
2017-01-13 08:13:21 +0900

    rename

--- machine-learning/mylib/softmax.py
+++ machine-learning/mylib/softmax.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def soft_max(x):
+def softmax(x):
     e_x = np.exp(x -np.max(x)) # prevent overflow
     if e_x.ndim == 1:
         y = e_x / np.sum(e_x)
@@ -11,11 +11,11 @@ def soft_max(x):
 
 if __name__ == '__main__':
     X = np.array([40, 10, 100]);
-    y = soft_max(X)
+    y = softmax(X)
     print(y)
     print(np.sum(y))
 
     X = np.array([[1, 10, 100], [53, 23, 30]]);
-    y = soft_max(X)
+    y = softmax(X)
     print(y)
     print(np.sum(y, axis=1)[:, np.newaxis])
